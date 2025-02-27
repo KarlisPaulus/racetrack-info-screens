a7d66ab84d7d73004f93d35f08bcdc7b1612490f
siimsiren <siim.siren@gmail.com>
2025-02-27 19:48:54 +0200

    Security frontend, backend tbd

--- Login_Page/login.js
+++ Login_Page/login.js
@@ -1,4 +1,3 @@
 const loginContainer = document.createElement('div'); // Make a box to put options in
     loginContainer.className = 'loginContainer';
     document.body.appendChild(loginContainer);
-

68c0988ed882d2f6e6127c325dce990a2c1d85b9
siimsiren <siim.siren@gmail.com>
2025-02-27 19:47:10 +0200

    Initial Security/Login commit

---
+++ Login_Page/login.js
@@ -0,0 +1,4 @@
+const loginContainer = document.createElement('div'); // Make a box to put options in
+    loginContainer.className = 'loginContainer';
+    document.body.appendChild(loginContainer);
+

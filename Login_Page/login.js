2ad40d65b222b31505b7f43fc12ca93c2a046775
siimsiren <siim.siren@gmail.com>
2025-02-28 19:00:31 +0200

    Created buttons

--- Login_Page/login.js
+++ Login_Page/login.js
@@ -1,3 +1,26 @@
 const loginContainer = document.createElement('div'); // Make a box to put options in
     loginContainer.className = 'loginContainer';
     document.body.appendChild(loginContainer);
+const spectator = document.createElement('button'); // Spectator button
+    spectator.className = 'spectator';
+    spectator.id = 'spectator';
+    spectator.textContent = 'Spectator';
+    loginContainer.appendChild(spectator);
+const laptimer = document.createElement('button'); // Lap timer button
+    laptimer.className = 'lapTimer';
+    laptimer.id = 'lapTimer';
+    laptimer.textContent = 'Lap Timer';
+    loginContainer.appendChild(laptimer);
+const raceControl = document.createElement('button'); // Race Control button
+    raceControl.className = 'raceControl';
+    raceControl.id = 'raceControl';
+    raceControl.textContent = 'Race Control';
+    loginContainer.appendChild(raceControl);
+const raceManagement = document.createElement('button'); // Race Management button
+    raceManagement.className = 'raceManagement';
+    raceManagement.id = 'raceManagement';
+    raceManagement.textContent = 'Race Management';
+    loginContainer.appendChild(raceManagement);
+
+
+
